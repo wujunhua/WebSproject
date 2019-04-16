a7521a9cba2feabeaeecd5a493ddb763a90b6334
gowiredu95 <48763819+gowiredu95@users.noreply.github.com>
2019-04-16 13:30:54 -0700

    Removed main method from PasswordGenerator

--- src/java/Controller/PasswordGenerator.java
+++ src/java/Controller/PasswordGenerator.java
@@ -1,25 +1,6 @@
 package Controller;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
-/**
- *
- * @author syntel
- */
 public class PasswordGenerator {
-
-    /*
-    public static void main(String[] args) {
-        // TODO code application logic here
-        PasswordGenerator passwordGenerator = new PasswordGenerator();       
-        passwordGenerator.generatePassword(); // output ex.: lrU12fmM 75iwI90o
-    }
-    */
     
     // **JUST CALL THIS METHOD TO GENERATE THE RANDOM PASSWORD. IT WILL TAKE CARE OF EVERYTHING.
     public String generatePassword()

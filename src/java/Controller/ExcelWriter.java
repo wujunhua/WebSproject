e196c0c75602be5242da44dd644c4be382889cba
Chris <Ian_Nielsen@syntelinc.com>
2019-04-19 17:48:56 -0700

    adding instructions function skeleton in place

--- src/java/Controller/ExcelWriter.java
+++ src/java/Controller/ExcelWriter.java
@@ -21,6 +21,7 @@ public class ExcelWriter {
     private final String scoreTag; // appended to each module name to communicate that it is a score column
     private OutputStream outputStream;
     private ArrayList<String> columnTitles;
+    private String[] instructions;
     
     /*
         constructor
@@ -38,6 +39,14 @@ public class ExcelWriter {
         columnTitles.add("Name");
         columnTitles.add("Email");
         columnTitles.add("Reporting Manager");
+        
+        // print these in the first rows
+        instructions = new String[] {
+            "Performica Data Entry Template",
+            "Please enter employee details and scores across a single row.",
+            "Score 1 is the first attempt, score 2 is the first retake, and score 3 is the second retake.",
+            "If retakes do not apply to the student, then leave those cells blank."
+        };
     }
     
     public void createExcelTemplateFile(ArrayList<String> moduleNames) {
@@ -55,6 +64,7 @@ public class ExcelWriter {
         XSSFCellStyle style = workbook.createCellStyle();
         style.setFont(boldFont); // bold font is used in this styling
         
+        addInstructionsToSpreadsheet(spreadsheet); // place instructions on spreadsheet
 
         int firstRowIndex = 0; // only writing to the first row
         XSSFRow row = spreadsheet.createRow(firstRowIndex); // title row, indicates the data to be entered
@@ -100,4 +110,23 @@ public class ExcelWriter {
         
         return scoreTitles;
     }
+
+    
+    private int addInstructionsToSpreadsheet(XSSFSheet spreadsheet) {
+    
+        int currentRow = (-1);
+        int FIRST_COL_INDEX = 0;
+        
+        // loop through instruction strings and rows together writing them in the first column
+        for(currentRow = 0; currentRow < instructions.length; currentRow++) {
+            
+            XSSFRow row = spreadsheet.createRow(currentRow);
+            Cell cell = row.createCell(FIRST_COL_INDEX);
+            
+            cell.setCellValue(instructions[currentRow]);
+        }
+        
+        return (currentRow + 1); // one index past the last instruction row
+    }
+    
 }

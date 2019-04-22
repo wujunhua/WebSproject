c85919211ea58eab8cdebdd490e185e74b8e140e
kbolling <48736855+kabollinger03@users.noreply.github.com>
2019-04-22 16:15:25 -0700

    Revert "UNDO THIS BEFORE MERGING"
    This reverts commit 9ef3dd0d8829bd03698a4f73b057d03ee787ea6d. Because it
    does configuration local to one machine.

--- src/java/Controller/SendEmail.java
+++ src/java/Controller/SendEmail.java
@@ -142,13 +142,13 @@ public class SendEmail {
             //add header image
             messageBodyPart = new MimeBodyPart();
             DataSource fds = new FileDataSource(
-            "C:\\trainingkb\\WebSproject\\web\\resources\\img\\header.jpg");
+            "header.jpg");
 
             messageBodyPart.setDataHandler(new DataHandler(fds));
             messageBodyPart.setHeader("Content-ID", "<image>");
             multipart.addBodyPart(messageBodyPart);
             messageBodyPart = new MimeBodyPart();
-            DataSource fds2 = new FileDataSource("C:\\trainingkb\\WebSproject\\web\\resources\\img\\welcome.jpg");
+            DataSource fds2 = new FileDataSource("welcome.jpg");
             messageBodyPart.setDataHandler(new DataHandler(fds2));
             messageBodyPart.setHeader("Content-ID", "<image2>");
             multipart.addBodyPart(messageBodyPart);
@@ -272,7 +272,7 @@ public class SendEmail {
             //add header image
             messageBodyPart = new MimeBodyPart();
             DataSource fds = new FileDataSource(
-            "C:\\trainingkb\\WebSproject\\web\\resources\\img\\header.jpg");
+            "header.jpg");
 
             messageBodyPart.setDataHandler(new DataHandler(fds));
             messageBodyPart.setHeader("Content-ID", "<image>");

b7c955f8db0a8be1c59be3dae64c9ad2294892e2
kbolling <48736855+kabollinger03@users.noreply.github.com>
2019-04-21 18:11:22 -0700

    Grab and siaplay categoreis from DB

--- src/java/Controller/PDF.java
+++ src/java/Controller/PDF.java
@@ -81,45 +81,42 @@ public class PDF {
 		this.filePath = filePath;
 	}
     
-	public void generate(String empid) throws Exception{ //empid not implemented yet, should be able to get everything you will need from it
-        //define output vars
-        ArrayList<String> stream = new ArrayList();
-        ArrayList<String> foundations = new ArrayList();
-        ArrayList<String> specializations = new ArrayList();
-        ArrayList<String> domains = new ArrayList();         
-        String name, avgGrade, fGrade, sGrade, dGrade;
-        System.out.println(empid + " EmpID");
+    public void generate(String empid) throws Exception{
         //init config
         njdbc = new NamedParameterJdbcTemplate(dataSource);
         PDFinfo info = new PDFinfo(dataSource.getConnection());
         info.setDataSource(dataSource);
         info.setNjdbc(njdbc);
-       
+        
         if(!info.getAllIds().contains(empid)){ //this is O(n^2) methinks. make the query return one think and do bool return.
             System.out.println("Employee ID not found. PDF has not been generated.");
             return;
         }
 
+        //define output vars
+        String name, avgGrade, fGrade, sGrade, dGrade;
+        
+        ArrayList<String> stream = new ArrayList();
+       
+        ArrayList<String[]> cats = new ArrayList<String[]>();
+        cats.addAll(info.getCategoryNameID());
+        
         //populate output variables
         name=info.getEmployeeName(empid);
         stream=info.getStreamIDName(empid);
         avgGrade=info.getAverageScore(empid);
-        foundations.addAll(info.getModuleScoresByCategory(empid, "FOUND01"));
-        specializations.addAll(info.getModuleScoresByCategory(empid, "SPEC01"));
-        domains.addAll(info.getModuleScoresByCategory(empid, "PD01"));
         fGrade=info.getAverageScoreByCategory(empid, "FOUND01");
         sGrade=info.getAverageScoreByCategory(empid, "SPEC01");
-        System.out.println("MAde it");
         dGrade=info.getAverageScoreByCategory(empid, "PD01");
-        
-        //define a <hr />
+		
+        //define a <hr /> --------------------------------
         char[] charArray = new char[1835];
         Arrays.fill(charArray, ' ');
         String hrHelp = new String(charArray);
         
         Paragraph hRule = new Paragraph();
-        hRule.addMarkup("__" + hrHelp + " __", 1, font); 
-        //because the "real way" is being diffuclt, cheat by adding a realy tiny empty string that is underlined.
+        hRule.addMarkup("__" + hrHelp + " __", 1, font);
+        //------------------------------------------------
         
         //create PDF
         float linspace = 12;
@@ -131,9 +128,11 @@ public class PDF {
         Document document = new Document(leftMargin, rightMargin, topMargin, bottomMargin);
         
         document.add(new ImageElement(imagePath), new VerticalLayoutHint(Alignment.Left, 0, 0,
-		0, 0, true));
+		0, 0, true)); 
         document.add(new VerticalSpacer(100));
-                        
+              
+        Paragraph par = new Paragraph();
+        
         Paragraph title = new Paragraph();
         title.addMarkup("{color:#0066a1}__***PERFORMICA REPORT***__", 20, font);
        	document.add(title, VerticalLayoutHint.CENTER);
@@ -148,7 +147,7 @@ public class PDF {
         //document.add(new VerticalSpacer(6.5f));
         
         document.add(new VerticalSpacer(2*sectionBreak));
-        System.out.println("Right Here");
+        
         Paragraph p1 = new Paragraph();
         p1.addMarkup("{color:#0066a1}__*My Trainings*__:", 12, font);
         document.add(p1);
@@ -173,42 +172,31 @@ public class PDF {
         document.add(new VerticalSpacer(linspace));
         */
         
-        document.add(hRule);
-        document.add(new VerticalSpacer(5));
-        Paragraph found = new Paragraph();
-        found.addMarkup("{color:#0066a1}*Foundations*{color:#000000}:  ", 11, font);
-        for(int i = 0; i < foundations.size() - 2; i+=2){
-            
-            found.addMarkup(foundations.get(i) + ",  ", 11, font);
-        }
-        found.addMarkup(foundations.get(foundations.size() - 2), 11, font);
-
-        document.add(found);
+        
+        
+        //modules
         document.add(hRule);
         document.add(new VerticalSpacer(linspace));
-
-        Paragraph spec = new Paragraph();
-        spec.addMarkup("{color:#0066a1}*Specializations*{color:#000000}: ", 11, font);
-        for(int i = 0; i < specializations.size() - 2; i+=2){
+        for (String[] cat : cats){
+            ArrayList<String> scores = new ArrayList<String>();
+            scores.addAll(info.getModuleScoresByCategory(empid, cat[1]));
             
-            spec.addMarkup(specializations.get(i) + ",  ", 11, font);
+            if (scores.size() > 0){
+                document.add(new VerticalSpacer(5));
+                par = new Paragraph();
+
+                par.addMarkup("{color:#0066a1}*" + cat[0] + "*{color:#000000}:  ", 11, font);
+                for(int i = 0; i < scores.size() - 2; i+=2){
+                    par.addMarkup(scores.get(i) + ",  ", 11, font);
+                }
+                par.addMarkup(scores.get(scores.size() - 2), 11, font);
+
+                document.add(par, VerticalLayoutHint.LEFT);
+                document.add(hRule);
+                document.add(new VerticalSpacer(linspace));
+            }
         }
-        
-        spec.addMarkup(specializations.get(specializations.size() - 2), 11, font);  
-        document.add(spec);
-        document.add(hRule);
-        document.add(new VerticalSpacer(linspace));
-        
-        Paragraph pd = new Paragraph();
-        pd.addMarkup("{color:#0066a1}*Process / Domain*{color:#000000}: ", 11, font);
-        for(int i = 0; i < domains.size() - 2; i+=2){
-            pd.addMarkup(domains.get(i) + ",  ", 11, font);
-        }
-        pd.addMarkup(domains.get(domains.size() - 2), 11, font);  
-        document.add(pd);
-        document.add(hRule);
-        document.add(new VerticalSpacer(sectionBreak));
-        
+                
         Paragraph p2 = new Paragraph();
         p2.addMarkup("{color:#0066a1}__*My Performence*__\n\n<<bargraph goes here>>", 12, font); //http://www.java2s.com/Code/Java/Chart/JFreeChartHorizontalBarChartDemo2.htm
         document.add(new VerticalSpacer(linspace));
